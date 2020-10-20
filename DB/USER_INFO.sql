f384cd9b10d495ced143d7541353e2239e6ac9b3
JupiterDai <13050@192.168.35.1>
2020-10-20 12:57:50 +0800

    ADD SQL

---
+++ DB/USER_INFO.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `USER_INFO` (
+  `ID` bigint NOT NULL AUTO_INCREMENT,
+  `PASSWORD` varchar(256) COLLATE utf8mb4_0900_as_cs NOT NULL,
+  `USERNAME` varchar(100) COLLATE utf8mb4_0900_as_cs NOT NULL,
+  `CREATE_AT` datetime NOT NULL,
+  `CREATE_BY` varchar(100) COLLATE utf8mb4_0900_as_cs NOT NULL,
+  `UPDATE_AT` datetime NOT NULL,
+  `UPDATE_BY` varchar(100) COLLATE utf8mb4_0900_as_cs NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs
