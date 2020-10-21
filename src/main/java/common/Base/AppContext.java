1210bb82dcaebc9fc743fdb94287106e5224fe45
JupiterDai <13050@192.168.35.1>
2020-10-21 09:50:17 +0800

    update

--- src/main/java/common/Base/AppContext.java
+++
@@ -1,5 +0,0 @@
-package common.Base;
-
-public class AppContext {
-
-}
--- src/main/java/common/Base/BasePO.java
+++
@@ -1,9 +0,0 @@
-package common.Base;
-
-import java.io.Serializable;
-
-@SuppressWarnings("serial")
-public class BasePO implements Serializable ,Cloneable {
-	
-
-}
