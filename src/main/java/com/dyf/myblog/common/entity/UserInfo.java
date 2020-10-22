700d5b46ba81c6b5ac5eef1456099fa6ca38af14
JupiterDai <13050@192.168.35.1>
2020-10-22 13:10:43 +0800

    update

--- src/main/java/com/dyf/myblog/common/entity/UserInfo.java
+++ src/main/java/com/dyf/myblog/common/entity/UserInfo.java
@@ -7,23 +7,25 @@ public class UserInfo extends BasePO {
 	private Long id;
 	private String passWord;
 	private String userName;
-	
-	public Long getID() {
+	public Long getId() {
 		return id;
-	} 
-	public void setID() {
-		this.id=id;
+	}
+	public void setId(Long id) {
+		this.id = id;
 	}
 	public String getPassWord() {
 		return passWord;
 	}
-	public void setPassword() {
-		this.passWord=passWord;
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
 	}
 	public String getUserName() {
 		return userName;
 	}
-	public void  setUserName() {
-		this.userName=userName;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
+	
+	
+	
 }
